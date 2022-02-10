49649a7aee02df5d6451b8802b98efb2e1cd1909
Raya <mail.raya15@gmail.com>
2022-02-10 23:06:54 +0700

    add Footer react component

---
+++ web-react/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <footer className="App-footer">
+      <small className="copyright">
+        {`created by `}
+        <b>
+          <a href="https://github.com/gitraya" target="_blank" rel="noreferrer">
+            gitraya
+          </a>
+        </b>
+        {`- devChallenges.io`}
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
