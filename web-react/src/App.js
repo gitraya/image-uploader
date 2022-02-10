51eba60a1b500cde6a9983fb763b5b8660d4824a
Raya <mail.raya15@gmail.com>
2022-02-10 23:07:13 +0700

    add Footer to the webpage

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
