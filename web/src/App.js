40f54f592ff03d9d82cd2898969529ef7389f068
Raya <mail.raya15@gmail.com>
2022-06-20 23:23:05 +0700

    add toast

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Loading from "./components/Loading";
+import Result from "./components/Result";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
   const [imageURL, setImageURL] = useState(null);
 
   const onSubmitImage = async (event) => {
@@ -14,6 +15,7 @@ const App = () => {
       const file = event.target.files[0];
 
       if (!file) return;
+
       setLoading(true);
       const body = new FormData();
       body.append("image", file);
@@ -21,24 +23,38 @@ const App = () => {
       const response = await axios.post("/api/images/cloud", body, {
         headers: { "Content-Type": "multipart/form-data" },
       });
-      const data = await response.json();
 
-      if (data.status !== "SUCCESS") {
-        throw new Error(data.error);
+      if (response.status !== 201) {
+        throw new Error(response.data.error);
       }
 
-      event.current.value = "";
-      setImageURL(data.imageURL);
+      event.target.value = "";
+      setImageURL(response.data.url);
       setLoading(false);
     } catch (error) {
-      setError(true);
       setLoading(false);
+      toast.error("An error occurred while uploading the image", {
+        className: "text-poppins font-normal",
+      });
     }
   };
 
   return (
-    <div className="App">
-      <main>{loading ? <Loading /> : <Form onSubmit={onSubmitImage} />}</main>
+    <div className="min-w-full min-h-screen flex flex-col relative">
+      <main>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            {imageURL ? (
+              <Result url={imageURL} />
+            ) : (
+              <Form onSubmit={onSubmitImage} />
+            )}
+          </>
+        )}
+      </main>
+      <Toaster />
       <Footer />
     </div>
   );
